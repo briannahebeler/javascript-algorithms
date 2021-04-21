ba4c8514b56ec51368bd9ca4616930cb77fd9083
Brianna Hebeler <briannahebeler@gmail.com>
2021-04-21 17:30:53 -0400

    added functions to each algorithm

--- arithmetic/script.js
+++ arithmetic/script.js
@@ -1,26 +1,59 @@
 // addition function //
 
-var addNum1 = document.getElementById(addNum1);
-var addNum2 = document.getElementById(addNum2);
-var addAns = document.getElementById(addAns);
-var addBtn = document.getElementById(addBtn);
+var addAns = document.getElementById("addAns");
+var addBtn = document.getElementById("addBtn");
+
+function addFunc() {
+    var num1 = JSON.parse(document.getElementById("addNum1").value);
+    var num2 = JSON.parse(document.getElementById("addNum2").value);
+    var ans = num1 + num2;
+    console.log(ans);
+    addAns.textContent = ans;
+}
+
+addBtn.addEventListener("click", addFunc);
 
 // subtraction function //
 
-var subNum1 = document.getElementById(subNum1);
-var subNum2 = document.getElementById(subNum2);
-var subAns = document.getElementById(subAns);
-var subBtn = document.getElementById(subBtn);
+var subAns = document.getElementById("subAns");
+var subBtn = document.getElementById("subBtn");
+
+function subFunc() {
+    var num1 = JSON.parse(document.getElementById("subNum1").value);
+    var num2 = JSON.parse(document.getElementById("subNum2").value);
+    var ans = num1 - num2;
+    console.log(ans);
+    subAns.textContent = ans;
+}
+
+subBtn.addEventListener("click", subFunc);
 
 // multiplication function //
-var multNum1 = document.getElementById(multNum1);
-var multNum2 = document.getElementById(multNum2);
-var multAns = document.getElementById(multAns);
-var multBtn = document.getElementById(multBtn);
+
+var multAns = document.getElementById("multAns");
+var multBtn = document.getElementById("multBtn");
+
+function multFunc() {
+    var num1 = JSON.parse(document.getElementById("multNum1").value);
+    var num2 = JSON.parse(document.getElementById("multNum2").value);
+    var ans = num1 * num2;
+    console.log(ans);
+    multAns.textContent = ans;
+}
+
+multBtn.addEventListener("click", multFunc);
 
 // division function //
 
-var divNum1 = document.getElementById(divNum1);
-var divNum2 = document.getElementById(divNum2);
-var divAns = document.getElementById(divAns);
-var divBtn = document.getElementById(divBtn);
+var divAns = document.getElementById("divAns");
+var divBtn = document.getElementById("divBtn");
+
+function divFunc() {
+    var num1 = JSON.parse(document.getElementById("divNum1").value);
+    var num2 = JSON.parse(document.getElementById("divNum2").value);
+    var ans = num1 * num2;
+    console.log(ans);
+    divAns.textContent = ans;
+}
+
+divBtn.addEventListener("click", divFunc);
