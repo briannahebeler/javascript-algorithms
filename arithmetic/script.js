141682966158505f7c2396730b0ab2df45203b2b
Brianna Hebeler <briannahebeler@gmail.com>
2021-04-21 16:08:44 -0400

    added elements as variables on js from html

--- arithmetic/script.js
+++ arithmetic/script.js
@@ -1,1 +1,26 @@
-// add function//
+// addition function //
+
+var addNum1 = document.getElementById(addNum1);
+var addNum2 = document.getElementById(addNum2);
+var addAns = document.getElementById(addAns);
+var addBtn = document.getElementById(addBtn);
+
+// subtraction function //
+
+var subNum1 = document.getElementById(subNum1);
+var subNum2 = document.getElementById(subNum2);
+var subAns = document.getElementById(subAns);
+var subBtn = document.getElementById(subBtn);
+
+// multiplication function //
+var multNum1 = document.getElementById(multNum1);
+var multNum2 = document.getElementById(multNum2);
+var multAns = document.getElementById(multAns);
+var multBtn = document.getElementById(multBtn);
+
+// division function //
+
+var divNum1 = document.getElementById(divNum1);
+var divNum2 = document.getElementById(divNum2);
+var divAns = document.getElementById(divAns);
+var divBtn = document.getElementById(divBtn);
