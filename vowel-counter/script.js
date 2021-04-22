36796fb100e5913ae2a35abdd5b64c078792cca2
Brianna Hebeler <briannahebeler@gmail.com>
2021-04-21 20:15:13 -0400

    added folder structure and wrote out skeleton for vowel counter

---
+++ vowel-counter/script.js
@@ -0,0 +1,11 @@
+var ans = document.getElementById("ans");
+var btn = document.getElementById("btn");
+
+function vowelCounter() {
+    var text = document.getElementById("text").value;
+
+    ans.textContent = "";
+    
+};
+
+btn.addEventListener("click", vowelCounter);
